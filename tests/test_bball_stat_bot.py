06b0df9eb456f0b90888546a82cfca6f6e24df2a
Garrett Bauer <bauer.garrett9@gmail.com>
2022-07-15 22:23:57 -0600

    getResponse test for two players

--- tests/test_bball_stat_bot.py
+++ tests/test_bball_stat_bot.py
@@ -52,28 +52,66 @@ def testGetStatsWrapperHappyPath(monkeypatch):
         )
     )
 
+
 def testGetResponseException(monkeypatch):
     def mockGetStats(name: str, playoffs: bool):
         return pd.DataFrame([])
-        
+
     monkeypatch.setattr("src.bball_stat_bot.get_stats", mockGetStats)
 
     with pytest.raises(stat_bot.PlayerNotFound):
         _ = stat_bot.getResponse("Test", "", ["SEASON", "AGE", "TEAM", "LEAGUE"], False)
 
+
 def testGetResponseOnePlayer(monkeypatch):
     def mockGetStats(name: str, playoffs: bool):
-       return pd.DataFrame(
+        return pd.DataFrame(
             {
                 "SEASON": ["2020-2021", "2021-2022"],
                 "AGE": [23.0, 24.0],
                 "TEAM": ["TOR", "TOR"],
                 "LEAGUE": ["NBA", "NBA"],
             }
-        ) 
+        )
 
     monkeypatch.setattr("src.bball_stat_bot.get_stats", mockGetStats)
 
     player_name = "Damian Lillard"
 
-    assert stat_bot.getResponse(player_name, "", ["SEASON", "AGE", "TEAM", "LEAGUE"], False) == [stat_bot.PlayerResponse("Damian Lillard", "SEASON|AGE|TEAM|LEAGUE\n-|-|-|-\n2020-2021|23.0|TOR|NBA\n2021-2022|24.0|TOR|NBA\n")]
+    assert stat_bot.getResponse(
+        player_name, "", ["SEASON", "AGE", "TEAM", "LEAGUE"], False
+    ) == [
+        stat_bot.PlayerResponse(
+            player_name,
+            "SEASON|AGE|TEAM|LEAGUE\n-|-|-|-\n2020-2021|23.0|TOR|NBA\n2021-2022|24.0|TOR|NBA\n",
+        )
+    ]
+
+
+def testGetResponseTwoPlayers(monkeypatch):
+    def mockGetStats(name: str, playoffs: bool):
+        return pd.DataFrame(
+            {
+                "SEASON": ["2020-2021", "2021-2022"],
+                "AGE": [23.0, 24.0],
+                "TEAM": ["TOR", "TOR"],
+                "LEAGUE": ["NBA", "NBA"],
+            }
+        )
+
+    monkeypatch.setattr("src.bball_stat_bot.get_stats", mockGetStats)
+
+    player_one, player_two = "Jusuf Nurkic", "Keljin Blevins"
+
+    assert stat_bot.getResponse(
+        player_one, player_two, ["SEASON", "AGE", "TEAM", "LEAGUE"], False
+    ) == [
+        stat_bot.PlayerResponse(
+            player_one,
+            "SEASON|AGE|TEAM|LEAGUE\n-|-|-|-\n2020-2021|23.0|TOR|NBA\n2021-2022|24.0|TOR|NBA\n",
+        ),
+        stat_bot.PlayerResponse(
+            player_two,
+            "SEASON|AGE|TEAM|LEAGUE\n-|-|-|-\n2020-2021|23.0|TOR|NBA\n2021-2022|24.0|TOR|NBA\n",
+        ),
+    ]
